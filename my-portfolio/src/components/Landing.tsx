0bfd00b84375b44fecc160af28d533f6516d0a9a
Alexa Sheldon <alexarsheldon@gmail.com>
2025-11-18 00:20:19 -0500

    Fixing colors on Landing and putting text on top

--- my-portfolio/src/components/Landing.tsx
+++ my-portfolio/src/components/Landing.tsx
@@ -17,8 +17,19 @@ function Landing() {
         minHeight: 200.00,
         minWidth: 200.00,
         scale: 1.00,
-        scaleMobile: 1.00
-        // colorMode: "variance"
+        scaleMobile: 1.00,
+        backgroundColor: 0x000000, // Dark greenish background
+        backgroundAlpha: 0.0,
+        color1: 0x6495ed, // Cornflower blue
+        color2: 0xc8d62b, // Bright yellow-green
+        birdSize: 1.50,
+        wingSpan: 20.00,
+        separation: 60.00,
+        alignment: 20.00,
+        cohesion: 20.00,
+        quantity: 4.00,
+        speedLimit: 4.00,
+        colorMode: "lerpGradient"
       }));
     }
     return () => {
@@ -28,6 +39,21 @@ function Landing() {
 
   return (
     <div ref={vantaRef} style={{ height: '100vh', width: '100%' }} id="landing">
+      <div
+        style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          color: '#3f523a',
+          textAlign: 'center',
+          fontSize: '2rem',
+          fontWeight: 'bold',
+        }}
+      >
+        <p>Hello, World!</p>
+        <p>My name is Alexa Sheldon</p>
+      </div>
     </div>);
 }
 
