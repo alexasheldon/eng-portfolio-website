fcfbe910ae279f7d5fe04e0562277d204b2a67cb
Alexa Sheldon <alexarsheldon@gmail.com>
2025-11-25 16:36:52 -0500

    Creating projects carousel

--- my-portfolio/src/components/Projects.tsx
+++ my-portfolio/src/components/Projects.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import imgSIP from '../assets/SIP.jpg';
 import imgPorridge from '../assets/IMG_6759.jpg';
 import pandasReportPdf from '../assets/CS4775_Final_Report_2024.pdf';
 import dashboardImg from '../assets/single-case-litigation.png'
 import aslImg from '../assets/o-asl.png';
+import redPandaFallback from "../assets/red_panda_fallback.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Thumbs, Mousewheel } from "swiper/modules";
+import "swiper/swiper-bundle.css";
 import '../styles/projects.css';
 
 interface ProjectsProps { }
 
+interface Project {
+  title: string;
+  description: string;
+  mainImage?: string;
+  additionalImage?: string;
+  embeds?: string[];
+  tags?: string[];
+  gitLink?: string;
+}
+
+const projects: Project[] = [
+  {
+    title: "Porridge - Cigna",
+    mainImage: imgSIP,
+    description: "Created a full end-to-end web app which analyzed code diffs from enterprise GitHub to gather insights on " +
+      "Pull Requests including how much to test and what the major changes were. (Won the Summer Innovations " +
+      "Project competition)",
+    additionalImage: imgPorridge,
+    tags: ["Node.js w/ bun", "Typescript", "RESTful APIs", "Git/GitHub", "Artifical Intelligence", "AWS", "PostreSQL"]
+  },
+  {
+    title: "Red Panda Phylogeny Analysis",
+    description: "Applying algorithms to Red Panda DNA to determine a potential split in its speciation.",
+    embeds: ["https://docs.google.com/presentation/d/e/2PACX-1vTuIOKVnPhrLvKjZPqffxpJSvHfo2Sj_8T4HXUbohAXMGJH-vFb6WViFar-onamWQ02_-px7rxNvBQZ/pubembed?start=false&loop=false&delayms=3000", pandasReportPdf],
+    tags: ["Python", "Sequence Alignment", "Phylogenetic Trees", "Algorithms", "Beta-Hill Climbing"],
+    gitLink: "https://github.com/alexasheldon/cs4775_final_project_phylogeny_red_pandas"
+  },
+  {
+    title: "Single Case Litigation Budget",
+    description: "Created a live PowerBI dashboard to assist with asset management & budget tracking. The live data which is confidential has been replaced with synthetic data.",
+    mainImage: dashboardImg,
+    tags: ["PowerBI", "Data Visualization", "DAX", "Data Analysis", "Asset Mangagement"]
+  }
+];
+
 const Projects: React.FC<ProjectsProps> = () => {
+  const [thumbsSwiper, setThumbsSwiper] = useState<any>(null);
   return (
     <section id="projects">
       <div className="w3-row-padding">
@@ -18,61 +58,78 @@ const Projects: React.FC<ProjectsProps> = () => {
         <div>
           <b><h3>My Projects</h3></b>
         </div>
-        <div className="w3-third w3-container w3-margin-bottom">
-          <div className="w3-container w3-white">
-            <h4><b>Porridge - Cigna</b></h4>
-            <a href={imgSIP} target="_blank">
-              <img src={imgSIP} alt="SIP Team 6" style={{ width: "100%" }} className="w3-hover-opacity" />
-            </a>
-            <p>
-              <img src={imgPorridge} alt="SIP Team 6" style={{ float: "right", width: "50%", margin: "0 0 1em 1em" }} />
-              Created a full end-to-end web app which analyzed code diffs from enterprise GitHub to gather insights on
-              Pull Requests including how much to test and what the major changes were. (Won the Summer Innovations
-              Project competition)
-            </p>
-            <div className="w3-tag w3-light-grey w3-small">Node.js w/ bun</div>
-            <div className="w3-tag w3-light-grey w3-small">Typescript</div>
-            <div className="w3-tag w3-light-grey w3-small">RESTful APIs</div>
-            <div className="w3-tag w3-light-grey w3-small">Git/Github</div>
-            <div className="w3-tag w3-light-grey w3-small">Artificial Intelligence</div>
-            <div className="w3-tag w3-light-grey w3-small">AWS</div>
-            <div className="w3-tag w3-light-grey w3-small">PostgreSQL</div>
-          </div>
-        </div>
-        <div className="w3-third w3-container">
-          <div className="w3-container w3-white">
-            <h4><b>Red Panda Phylogeny Analysis</b></h4>
-            <a href="https://github.com/alexasheldon/cs4775_final_project_phylogeny_red_pandas" target="_blank"><i className="fa fa-github fa-2x w3-hover-opacity"></i></a>
-            <p>Applying algorithms to Red Panda DNA to determine a potential split in its speciation.</p>
-            <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTuIOKVnPhrLvKjZPqffxpJSvHfo2Sj_8T4HXUbohAXMGJH-vFb6WViFar-onamWQ02_-px7rxNvBQZ/pubembed?start=false&loop=false&delayms=3000"
-              width="100%" height="250px" allowFullScreen style={{ border: "none" }}>
-            </iframe>
-            <iframe src={pandasReportPdf} width="100%" height="300px">
-              This browser does not support PDFs. Please download the PDF to view it:
-              <a href={pandasReportPdf}>Download PDF</a>.
-            </iframe>
-            <div className="w3-tag w3-light-grey w3-small">Python</div>
-            <div className="w3-tag w3-light-grey w3-small">Sequence Alignment</div>
-            <div className="w3-tag w3-light-grey w3-small">Phylogenetic Trees</div>
-            <div className="w3-tag w3-light-grey w3-small">Algorithmic Implementation</div>
-          </div>
-        </div>
-        <div className="w3-third w3-container w3-margin-bottom">
-          <div className="w3-container w3-white">
-            <h4><b>Single Case Litigation Budget Tracking Dashboard - Fortress</b></h4>
-            <a href={dashboardImg} target="_blank">
-              <img src={dashboardImg} alt="A PowerBI Dashboard report for Budget Tracking"
-                style={{ width: "100%" }} className="w3-hover-opacity" />
-            </a>
-            <p>Created a live PowerBI dashboard to assist with asset management & budget tracking. The live data which
-              is
-              confidential has been replaced with synthetic data.</p>
-            <div className="w3-tag w3-light-grey w3-small">PowerBI</div>
-            <div className="w3-tag w3-light-grey w3-small">Data Visualization</div>
-            <div className="w3-tag w3-light-grey w3-small">DAX</div>
-            <div className="w3-tag w3-light-grey w3-small">Data Analysis</div>
-            <div className="w3-tag w3-light-grey w3-small">Asset Management</div>
-          </div>
+        <div className="projects-section">
+          <Swiper
+            modules={[Navigation, Pagination, Thumbs, Mousewheel]}
+            navigation
+            pagination={{ clickable: true }}
+            spaceBetween={30}
+            slidesPerView={1}
+            thumbs={{ swiper: thumbsSwiper }}
+            mousewheel={{ forceToAxis: true }}
+          >
+            {projects.map((p, i) => (
+              <SwiperSlide key={i}>
+                <div className="project-card">
+                  <h3>{p.title}</h3>
+                  <p>
+                    {p.mainImage && <img src={p.mainImage} alt={p.title} className="main-img" />}
+                    {p.description}
+                    {p.additionalImage && <img src={p.additionalImage} alt="inline" className="inline-img" />}
+                  </p>
+                  {/* Embedded content (paper/slideshow) */}
+                  {p.embeds && p.embeds.length > 0 && (
+                    <div className="embed-row">
+                      {p.embeds.map((src, j) => (
+                        <iframe
+                          key={j}
+                          src={src}
+                          className="embed-item"
+                          allowFullScreen
+                          title={`${p.title}-embed-${j}`}
+                        ></iframe>
+                      ))}
+                    </div>
+                  )}
+                  {/* GitHub link */}
+                  {p.gitLink && (
+                    <a href={p.gitLink} target="_blank" rel="noopener noreferrer">
+                      <i className="fa fa-github fa-2x w3-hover-opacity"></i>
+                    </a>
+                  )}
+                  {/* Tags */}
+                  <div className="tags">
+                    {p.tags && p.tags.map((tag, j) => (
+                      <span key={j} className="tag">{tag}</span>
+                    ))}
+                  </div>
+
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <Swiper
+            modules={[Thumbs]}
+            onSwiper={setThumbsSwiper}
+            spaceBetween={10}
+            slidesPerView="auto"
+            watchSlidesProgress
+            className="thumbs-swiper"
+          >
+            {projects.map((p, i) => (
+              <SwiperSlide key={i}>
+                <div className="thumb-card">
+                  {p.mainImage ? (
+                    <img src={p.mainImage} alt={p.title} title={p.title} className="thumb-media" />
+                  ) : p.title == "Red Panda Phylogeny Analysis" ? (
+                    <img src={redPandaFallback} alt={p.title} title={p.title} className="thumb-media" />
+                  ) : (
+                    <div className="thumb-placeholder">{p.title}</div>
+                  )}
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </div>
       <div className="w3-row-padding">
@@ -85,29 +142,33 @@ const Projects: React.FC<ProjectsProps> = () => {
             <p><img src={aslImg} alt="The letter O in Sign Language"
               style={{ float: "right", width: "30%", margin: "0 0 1em 1em" }} />
               Implementing and training a CNN for recognizing American Sign Language letters in real time.</p>
-            <div className="w3-tag w3-light-grey w3-small">Python</div>
-            <div className="w3-tag w3-light-grey w3-small">CNNs</div>
-            <div className="w3-tag w3-light-grey w3-small">Computer Vision</div>
-            <div className="w3-tag w3-light-grey w3-small">Data Cleaning</div>
+            <div className="tag w3-light-grey w3-small">Python</div>
+            <div className="tag w3-light-grey w3-small">CNNs</div>
+            <div className="tag w3-light-grey w3-small">Computer Vision</div>
+            <div className="tag w3-light-grey w3-small">Data Cleaning</div>
           </div>
         </div>
         <div className="w3-third w3-container w3-margin-bottom">
           <div className="w3-container w3-white">
             <h4><b>Blackjack RL Agent</b></h4>
             <p>Implementing a reinforcement learning agent optimized for playing blackjack.</p>
-            <div className="w3-tag w3-light-grey w3-small">Python</div>
-            <div className="w3-tag w3-light-grey w3-small">Reinforcement Learning</div>
-            <div className="w3-tag w3-light-grey w3-small">DQN</div>
-            <div className="w3-tag w3-light-grey w3-small">AI Communication</div>
+            <div className="tag w3-light-grey w3-small">Python</div>
+            <div className="tag w3-light-grey w3-small">Reinforcement Learning</div>
+            <div className="tag w3-light-grey w3-small">DQN</div>
+            <div className="tag w3-light-grey w3-small">AI Communication</div>
           </div>
         </div>
         <div className="w3-third w3-container">
           <div className="w3-container w3-white">
-            <a href="https://github.com/alexasheldon/eng-portfolio-website" target='_blank'><h4><b>Portfolio Website</b></h4></a>
-            <p>This involves the creation and updating of this website!</p>
-            <div className="w3-tag w3-light-grey w3-small">React.js / Node.js</div>
-            <div className="w3-tag w3-light-grey w3-small">Typescript</div>
-            <div className="w3-tag w3-light-grey w3-small">HTML</div>
+            <h4><b>Portfolio Website</b></h4>
+            <p>This involves the creation and updating of this website!
+              <a href="https://github.com/alexasheldon/eng-portfolio-website" target="_blank" rel="noopener noreferrer">
+                <i className="fa fa-github fa-2x w3-hover-opacity" style={{ marginLeft: '6px' }}></i>
+              </a>
+            </p>
+            <div className="tag w3-light-grey w3-small">React.js / Node.js</div>
+            <div className="tag w3-light-grey w3-small">Typescript</div>
+            <div className="tag w3-light-grey w3-small">HTML</div>
           </div>
         </div>
       </div>
